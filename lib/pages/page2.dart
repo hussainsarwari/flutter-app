e09cf69d53926c32e73e46a75b9facad32db5fc9
hussainsarwari <sarwarihussain7@gmail.com>
2025-05-07 19:09:31 +0430

    add more pages

---
+++ lib/pages/page2.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+
+class Nextpage extends StatelessWidget {
+  const Nextpage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Next 2",style: TextStyle(
+          color: Colors.white,
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+        ),),
+        backgroundColor: const Color.fromARGB(255, 46, 42, 58),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/profile');
+          },
+          child: const Text("Go to Profile"),
+        ),
+      ),
+    );
+  }
+}
