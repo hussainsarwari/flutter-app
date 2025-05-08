e8a4817c0a6aa6f095495238e456a6f6ff779cd6
hussainsarwari <sarwarihussain7@gmail.com>
2025-05-08 12:15:17 +0430

    Auto commit changes

--- lib/pages/page2.dart
+++ lib/pages/page2.dart
@@ -51,7 +51,7 @@ class Nextpage extends StatelessWidget {
                     style: TextStyle(color: Colors.white),
                   ),
                 ),
-                const SizedBox() 
+                const SizedBox(wid) 
                 OutlinedButton(
                   style: OutlinedButton.styleFrom(
                     shape: RoundedRectangleBorder(
