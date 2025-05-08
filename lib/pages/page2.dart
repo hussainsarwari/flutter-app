7986c54fef17a2240bffec3699b3da56bce80342
hussainsarwari <sarwarihussain7@gmail.com>
2025-05-08 12:16:56 +0430

    Auto commit changes

--- lib/pages/page2.dart
+++ lib/pages/page2.dart
@@ -53,7 +53,7 @@ class Nextpage extends StatelessWidget {
                   )
                   ,
                   onPressed: () {
-                    Na
+                    Naviga
                   },
                   child: Text(
                     "Previous page",
