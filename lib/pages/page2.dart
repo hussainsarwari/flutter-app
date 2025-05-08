25b92b06947c3e9cb75433b0965b965abd6e86a7
hussainsarwari <sarwarihussain7@gmail.com>
2025-05-08 12:31:00 +0430

    Auto commit changes

--- lib/pages/page2.dart
+++ lib/pages/page2.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Nextpage extends StatelessWidget {
-  const Nextpage({super.key});
+  // const Nextpage({super.key});
 
   @override
   Widget build(BuildContext context) {
