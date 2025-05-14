a16c48af6fc6891d581a0c13c8e2061654aeff2c
hussainsarwari <sarwarihussain7@gmail.com>
2025-05-14 08:33:47 +0430

    Auto commit changes

--- lib/pages/app3.dart
+++ lib/pages/app3.dart
@@ -43,7 +43,7 @@ class _App3State extends State<App3> {
                   ElevatedButton(
                     onPressed: () {
                       quistionIndex++;
-                      if(qu)
+                      if(questionIde)
                     },
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.green,
