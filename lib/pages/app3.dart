51972f1d1caa6d2e817ea9e2273ba22fd28e2ef1
hussainsarwari <sarwarihussain7@gmail.com>
2025-05-14 09:14:21 +0430

    Auto commit changes

--- lib/pages/app3.dart
+++ lib/pages/app3.dart
@@ -67,7 +67,7 @@ class _App3State extends State<App3> {
                   const SizedBox(width: 20.0),
                   ElevatedButton(
                     onPressed: () {
-                      bool currentAnswer = questionIndex[questionIndex].answers;
+                      bool currentAnswer = question[questionIndex].answers;
                       if (currentAnswer) {
                         icons.add(Icon(Icons.check, color: Colors.green));
                       } else {
