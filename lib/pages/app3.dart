ea729d86e8e3e2805aa6962ec3768e2863f2ff1f
hussainsarwari <sarwarihussain7@gmail.com>
2025-05-14 08:43:36 +0430

    Commit #227: Automated commit

--- lib/pages/app3.dart
+++ lib/pages/app3.dart
@@ -66,7 +66,7 @@ class _App3State extends State<App3> {
                     onPressed: () {
                       setState((){
                         questionIndex ++;
-                        
+                        if
                       })
                     },
                     style: ElevatedButton.styleFrom(
