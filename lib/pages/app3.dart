31f1037743e241b00879ce1104490ac1e261d0fa
hussainsarwari <sarwarihussain7@gmail.com>
2025-05-14 09:11:59 +0430

    Auto commit changes

--- lib/pages/app3.dart
+++ lib/pages/app3.dart
@@ -19,7 +19,7 @@ class _App3State extends State<App3> {
   ];
   int questionIndex = 0;
   List<bool> answers = [true, true, true, false, true];
-  
+  list
   @override
   Widget build(BuildContext context) {
     return Scaffold(
