54cb29ea26abb4188494dea1aef9acff9b490703
hussainsarwari <sarwarihussain7@gmail.com>
2025-05-14 08:49:20 +0430

    Commit #259: Automated commit

--- lib/pages/app3.dart
+++ lib/pages/app3.dart
@@ -52,7 +52,7 @@ class _App3State extends State<App3> {
                 children: [
                   ElevatedButton(
                     onPressed: () {
-                      bool cu
+                      bool curre
                       setState(() {
                         questionIndex++;
                         if (questionIndex >= questions.length) {
