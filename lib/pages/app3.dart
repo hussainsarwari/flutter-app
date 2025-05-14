d170c48942e857a43ef0e27f823b1ae74655812b
hussainsarwari <sarwarihussain7@gmail.com>
2025-05-14 10:00:00 +0430

    Auto commit changes

--- lib/pages/app3.dart
+++ lib/pages/app3.dart
@@ -69,7 +69,7 @@ class _App3State extends State<App3> {
                       }
                       setState(() {
                         questionIndex++;
-                        if (questionIndex >= brain.getQuestion().length) {
+                        if (questionIndex >= brain.) {
                           questionIndex = 0;
                           icons.clear();
                         }
