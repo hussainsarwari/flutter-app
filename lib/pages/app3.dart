5757d06b3e90b3d69ec7940bd0b024930f22718a
hussainsarwari <sarwarihussain7@gmail.com>
2025-05-14 08:26:34 +0430

    Auto commit changes

--- lib/pages/app3.dart
+++ lib/pages/app3.dart
@@ -42,7 +42,7 @@ class _App3State extends State<App3> {
                   ElevatedButton(
                     onPressed: () {
                       setState(() {
-                        qu
+                        queti
                       });
                     },
                     style: ElevatedButton.styleFrom(
