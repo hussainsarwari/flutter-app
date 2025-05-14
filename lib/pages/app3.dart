34a0bd5f4664bb9e536a4f919a9591d459d4ef84
hussainsarwari <sarwarihussain7@gmail.com>
2025-05-14 09:56:46 +0430

    Auto commit changes

--- lib/pages/app3.dart
+++ lib/pages/app3.dart
@@ -25,7 +25,7 @@ class _App3State extends State<App3> {
             children: [
               const SizedBox(height: 20.0),
               Text(
-                brain.,
+                brain.getQuestion(),
                 style: TextStyle(color: Colors.white),
               ),
 
