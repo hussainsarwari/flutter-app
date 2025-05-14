e6fc106e380616077c8027ae5259c522e2d7bb16
hussainsarwari <sarwarihussain7@gmail.com>
2025-05-14 09:53:07 +0430

    Auto commit changes

--- lib/pages/components/barin.dart
+++ lib/pages/components/barin.dart
@@ -7,6 +7,7 @@ class Brain {
     Questions(question: "Is Flutter only for mobile apps?", answers: false),
     Questions(question: "Is Flutter open-source?", answers: true),
   ];
+  
 
   getQuestion(){
     return _questionList[questionIndex];
