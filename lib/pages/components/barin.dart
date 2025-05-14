89a591175b73c0557e2e076f26c1dc65c2ff638d
hussainsarwari <sarwarihussain7@gmail.com>
2025-05-14 09:48:39 +0430

    Auto commit changes

--- lib/pages/components/barin.dart
+++ lib/pages/components/barin.dart
@@ -1,4 +1,10 @@
 import 'questions.dart';
 class Barin {
-  
+   List<Questions> questionList = [
+    Questions(question: "Is Flutter a UI toolkit?", answers: true),
+    Questions(question: "Is Dart a programming language?", answers: true),
+    Questions(question: "Is Flutter used for web development?", answers: true),
+    Questions(question: "Is Flutter only for mobile apps?", answers: false),
+    Questions(question: "Is Flutter open-source?", answers: true),
+  ];
 }
