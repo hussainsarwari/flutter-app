599495e230259c6e6fe9a43bded8fe8463c3c43e
hussainsarwari <sarwarihussain7@gmail.com>
2025-05-14 09:50:05 +0430

    Auto commit changes

--- lib/pages/app3.dart
+++ lib/pages/app3.dart
@@ -35,7 +35,7 @@ class _App3State extends State<App3> {
                 children: [
                   ElevatedButton(
                     onPressed: () {
-                      bool currentAnswer = questionList[questionIndex].answers;
+                      bool currentAnswer = brainquestionList[questionIndex].answers;
                       if (currentAnswer) {
                         icons.add(Icon(Icons.check, color: Colors.green));
                       } else {
