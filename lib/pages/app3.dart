05877bfb1e21c06b15cd65a93696a91936524bf5
hussainsarwari <sarwarihussain7@gmail.com>
2025-05-14 08:50:38 +0430

    Commit #293: Automated commit

--- lib/pages/app3.dart
+++ lib/pages/app3.dart
@@ -48,7 +48,7 @@ class _App3State extends State<App3> {
                     onPressed: () {
                       bool currentAnswer = answers[questionIndex];
                       if(currentAnswer){
-                        icons.add(Icon(Icons.check,co))
+                        icons.add(Icon(Icons.check,color))
                       }
                       setState(() {
                         questionIndex++;
