347ba7904113550d1a5260d512d969b760b9550a
hussainsarwari <sarwarihussain7@gmail.com>
2025-05-14 08:42:01 +0430

    Auto commit changes

--- lib/pages/app3.dart
+++ lib/pages/app3.dart
@@ -45,7 +45,7 @@ class _App3State extends State<App3> {
                 children: [
                   ElevatedButton(
                     onPressed: () {
-                      useState(() {
+                      (() {
                         questionIndex = 2;
                         if (questionIndex >= questions.length) {
                           questionIndex = 0;
